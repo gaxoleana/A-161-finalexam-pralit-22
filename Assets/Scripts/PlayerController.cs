194f199057cc4759c7530c91eb9d65f249cbf73e
gaxoleana <75365996+gaxoleana@users.noreply.github.com>
2025-12-02 09:50:48 +0700

    PlayerController

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float moveSpeed = 5f;
+    private float jumpForce = 10f;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        float moveInput = Input.GetAxisRaw("Horizontal");
+        rb.linearVelocity = new Vector2 (moveInput * moveSpeed, rb.linearVelocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+        }
+    }
+}
